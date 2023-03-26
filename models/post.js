2668fb75cc5036a5de198970a1758a7bd55ac216
Vishaveet <vishvajeetgupta196@gmail.com>
2023-03-26 20:53:37 +0530

    Create a comment Schema in moduel

--- models/post.js
+++ models/post.js
@@ -8,7 +8,12 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    // include the array of ids of all comments in this post Schema itself 
+    comments :[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'comment'
+    }]
 } ,   
     {
         timestamps: true
--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const postsController=require('../controllers/post_controller');
 const passport=require('passport');
+
 router.post('/create',passport.checkAuthentication,postsController.create);
 // router.post('/create', function(req, res){   postsController.create });
 
